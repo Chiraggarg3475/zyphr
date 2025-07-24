177b0608696316ca986dc35bdd3ddaf3e63bf708
Chiraggarg3475 <gargfamily2004@gmail.com>
2025-07-24 11:32:16 +0530

    16: authentication

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -15,6 +15,7 @@ import { Button } from "@/components/ui/button";
 import {Form, FormField} from "@/components/ui/form";
 import { useRouter } from "next/navigation";
 import { PROJECT_TEMPLATES } from "../../constants";
+import { useClerk } from "@clerk/nextjs";
 
 
 
@@ -27,6 +28,7 @@ const formSchema = z.object({
 export const ProjectForm = () => {
     const router = useRouter();
     const trpc = useTRPC();
+    const clerk = useClerk();
     const queryClient = useQueryClient();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -44,8 +46,14 @@ export const ProjectForm = () => {
             //  TODO: Invalidate usuage status
         },
         onError: (error) => {
-            // TODO: Redirect to pricing page if specific error
             toast.error(error.message);
+            
+            if(error.data?.code === "UNAUTHORIZED"){
+                clerk.openSignIn();
+            }
+
+            // TODO: Redirect to pricing page if specific error 
+            
         }
     }));
 
